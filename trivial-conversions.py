33f10bc5ebfadf6f07a1495c7493db5d1f158af9
Sierra <60443752+ngmaihuong@users.noreply.github.com>
2020-03-25 13:18:00 -0400

    Update trivial-conversions.py

--- trivial-conversions.py
+++ trivial-conversions.py
@@ -7,4 +7,12 @@ while True:
     number = int(value)
     print(number, "minutes is", number / 60, "hours")
 
-  
+# Fahrenheit to Celcius
+
+while True:
+    value = input("Degree Fahrenheit to Celcius, please [q to quit]: ")
+    if value == 'q':
+        break
+    number = int(value)
+    print(number, "degree Fahrenheit is", (number - 32) * (5/9), "degree Celcius")
+    #How do I include a prompt to choose which side I want to convert?
