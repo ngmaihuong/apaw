7b3bf9ad8d98bc01fd067663461b3728388e422e
Sierra <60443752+ngmaihuong@users.noreply.github.com>
2020-03-25 13:43:13 -0400

    Update trivial-conversions.py

--- trivial-conversions.py
+++ trivial-conversions.py
@@ -3,35 +3,34 @@ while True:
     prompt = input("Minutes to hours [please type 1] or Hours to minutes [please type 2], please [q to quit]: ")
     if prompt == 'q':
         break
-    if prompt == "1":
+    while prompt == "1":
         value = input("Minutes to hours, please [q to quit]: ")
         if value == 'q':
             break
         number = int(value)
         print(number, "minutes is", number / 60, "hours")
-    if prompt == "2":
+    while prompt == "2":
         value = input("Hours to minutes, please [q to quit]: ")
         if value == 'q':
             break
         number = int(value)
         print(number, "hours is", number * 60, "minutes")
-#See question below.
 
 # Fahrenheit to Celcius
 while True:
     prompt = input("F to C [please type 1] or C to F [please type 2], please [q to quit]: ")
     if prompt == 'q':
         break
-    if prompt == "1":
+    while prompt == "1":
         value = input("Degree Fahrenheit to Celcius, please [q to quit]: ")
         if value == 'q':
             break
         number = int(value)
         print(number, "degree Fahrenheit is", (number - 32) * (5/9), "degree Celcius")
-    if prompt == "2":
+    while prompt == "2":
         value = input("Degree Celcius to Fahrenheit, please [q to quit]: ")
         if value == 'q':
             break
         number = int(value)
         print(number, "degree Celcius is", number / (5/9) + 32, "degree Fahrenheit")
-#How do I continue with my conversion choice without having to specify the prompt after each conversion?
+#How do I continue with my conversion choice without having to specify the prompt after each conversion? SOLVED- Change "if" to "while"
