6a4b2947d1576d71005b25dbe1d4a65e09022eb7
Sierra <60443752+ngmaihuong@users.noreply.github.com>
2020-03-25 14:58:54 -0400

    Create guessing-game.py

---
+++ guessing-game.py
@@ -0,0 +1,18 @@
+# In case you are too bored hanging out with yourself, here is how you can hang out with your computer...
+
+import random
+help(random) #I have no clue how to use this thing... Gotta look it up!
+
+secret = random.randrange(1000)
+
+while True:
+    guess = input("Guess what number your cute PC is having in mind, please [q to quit]: ")
+    if guess == 'q':
+        break
+    number = int(guess)
+    if number < secret:
+        print("Too low!")
+    elif number > secret:
+        print("Too high!")
+    else:
+        print("Yay! You got it!!")
