90e3236a2bae6c22b734fd2f3c2a3a45f9913802
Sierra <60443752+ngmaihuong@users.noreply.github.com>
2020-03-25 13:30:15 -0400

    Update trivial-conversions.py

--- trivial-conversions.py
+++ trivial-conversions.py
@@ -1,5 +1,4 @@
 # Minutes to hours
-
 while True:
     value = input("Minutes to hours, please [q to quit]: ")
     if value == 'q':
@@ -8,11 +7,20 @@ while True:
     print(number, "minutes is", number / 60, "hours")
 
 # Fahrenheit to Celcius
-
 while True:
-    value = input("Degree Fahrenheit to Celcius, please [q to quit]: ")
-    if value == 'q':
+    prompt = input("F to C [please type 1] or C to F [please type 2], please [q to quit]: ")
+    if prompt == 'q':
         break
-    number = int(value)
-    print(number, "degree Fahrenheit is", (number - 32) * (5/9), "degree Celcius")
-    #How do I include a prompt to choose which side I want to convert?
+    if prompt == "1":
+        value = input("Degree Fahrenheit to Celcius, please [q to quit]: ")
+        if value == 'q':
+            break
+        number = int(value)
+        print(number, "degree Fahrenheit is", (number - 32) * (5/9), "degree Celcius")
+    if prompt == "2":
+        value = input("Degree Celcius to Fahrenheit, please [q to quit]: ")
+        if value == 'q':
+            break
+        number = int(value)
+        print(number, "degree Celcius is", number / (5/9) + 32, "degree Fahrenheit")
+#How do I continue with my conversion choice without having to specify the prompt after each conversion?
